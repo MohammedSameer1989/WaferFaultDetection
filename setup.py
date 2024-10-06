745927465b702064d1040a3b58b8dd56bd97d541
MohammedSameer1989 <sameerjaipur24@gmail.com>
2024-10-06 16:37:27 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Waferfault",
+    version="0.0.1",
+    author="Sameer",
+    author_email="sameerjaipur24@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
